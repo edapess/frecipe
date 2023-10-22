6b04951cabee3844e330630619fad41fc8dd3fe4
edapess <edmondavetisyanw@gmail.com>
2023-10-23 00:59:24 +0400

    feat: nest with mysql, testing

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
+  }
+}
